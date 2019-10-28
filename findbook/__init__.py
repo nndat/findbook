e75a250f1ef285d008b3218cde4f7c7737e34211
nndat <nguyengocdat90@gmail.com>
2019-10-28 15:14:40 +0700

    update route

--- findbook/__init__.py
+++ findbook/__init__.py
@@ -9,12 +9,12 @@ app.config['SECRET_KEY'] = 'fb5524f179bc4dd364a2d22e4a0fc5af0'\
                             'fe6f17ba102bb7e6314ab19a3cd7d41'
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    books = None
-    if request.method == 'POST':
-        bookname = request.form.get('book').strip()
+@app.route('/')
+@app.route('/search')
+def search():
+    books = []
+    bookname = request.args.get('q')
+    if bookname:
         books = get_books(bookname)
         if books is None:
             flash(f'Không tìm thấy tên sách: "{bookname}".'
